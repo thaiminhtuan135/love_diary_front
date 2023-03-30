b9098ba08e07ad7ff7436ffd68c2583c69197041
thaiminhtuan135 <tuana11ht@gmail.com>
2023-03-30 15:04:14 +0700

    add prettierrc code

--- src/pages/admin/dashboard.tsx
+++ src/pages/admin/dashboard.tsx
@@ -223,7 +223,7 @@ function Dashboard(props:PropsWithChildren) {
                             className="hover:ml-4 justify-end pr-5 text-white hover:text-purple-500 dark:hover:text-blue-500 w-full bg-[#1E293B] p-3 rounded-full transform ease-in-out duration-300 flex">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                                  stroke="currentColor" className="w-4 h-4">
-                                <path stroke-linecap="round" stroke-linejoin="round"
+                                <path stroke-linecap ="round" stroke-linejoin="round"
                                       d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"/>
                             </svg>
                         </div>
